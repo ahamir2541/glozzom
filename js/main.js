89b285da57b86ccc1a6718d41beee295db391498
amir <amir2541@gmail.com>
2020-03-26 00:01:50 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$('.carousel').carousel({
+    interval: 5000
+})
+
+// sticky menu
+$(document).ready(function () {
+    $(window).bind('scroll', function () {
+        var navHeight = 300; // custom nav height
+        ($(window).scrollTop() > navHeight) ? $('nav').addClass('goToTop'): $('nav').removeClass('goToTop');
+    });
+});
